c06ee5eaa084cb3ec1e9d09913ad1243ae72ad2c
jerryhuang3 <jerryhuang@utexas.edu>
2019-04-24 02:08:53 +0000

    deployment db seeds and sendgrid config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
 
   # Order mailing
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'https://tinyjungle.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
